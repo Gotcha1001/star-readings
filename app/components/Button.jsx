07e22a02bd68dfc939e6e591f24b73bec2ad081a
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2025-06-04 16:39:56 +0200

    Readings

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button({ text, onClick, disabled }) {
         <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
-            className="bg-purple-600 text-white px-6 py-3 rounded-lg font-bold m-2 disabled:opacity-50 disabled:bg-indigo-600 "
+            className="bg-purple-700 text-white px-6 py-3 rounded-lg font-bold m-2 disabled:opacity-50 disabled:bg-indigo-600 "
             onClick={onClick}
             disabled={disabled}
         >
